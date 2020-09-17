8f8a87713f8e8410b1c5945b98dc8cbd115230f2
andrew_kandzuba <andrey.kandzuba@gmail.com>
2020-09-17 08:19:14 -0500

    comment out jar mv

--- cloudbuild/build.sh
+++ cloudbuild/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 mvn install -U -P !build-extras -DskipTests=true -Dmaven.javadoc.skip=true -B -V
-cp cli/target/*.jar /jar/
-ls /jar/
+#cp pipeline/target/*.jar /jar/
+#ls /jar/
