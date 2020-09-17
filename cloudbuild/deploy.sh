a6b9c4dab2c74e4ce14c1e121a2bc81d8753a664
andrew_kandzuba <andrey.kandzuba@gmail.com>
2020-09-17 09:42:06 -0500

    fix jar name

--- cloudbuild/deploy.sh
+++ cloudbuild/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 gpg --fast-import /root/.gpg/gpg.key
-cp /jar/cli-*.jar cli/target/
+cp /jar/pipeline-*.jar cli/target/
 mvn deploy -P sign,build-extras,docker -DskipTests=true --settings cd/mvnsettings.xml
