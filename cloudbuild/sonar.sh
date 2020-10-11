838bb4f800161982a314ea9ca7cc48beedc36f80
andrew_kandzuba <andrey.kandzuba@gmail.com>
2020-10-11 18:37:31 -0500

    fix sonar login

--- cloudbuild/sonar.sh
+++ cloudbuild/sonar.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-mvn clean scoverage:check scoverage:report sonar:sonar -Dsonar.login=$$_SONAR_TOKEN_SECRET -Dsonar.branch.name=$BRANCH_NAME -Dscoverage.minimumCoverage=$_MIN_COVERAGE -B --fail-at-end
+export SONAR_TOKEN=$(echo $$_SONAR_TOKEN_SECRET)
+mvn clean scoverage:check scoverage:report sonar:sonar -Dsonar.branch.name=$BRANCH_NAME -Dscoverage.minimumCoverage=$_MIN_COVERAGE -B --fail-at-end
