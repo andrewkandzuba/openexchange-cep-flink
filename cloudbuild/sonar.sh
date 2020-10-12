858905cee7a082c556e3ef59917570924bcc220a
andrew_kandzuba <andrey.kandzuba@gmail.com>
2020-10-12 15:34:37 -0500

    remove scoverage autocheck from pom

--- cloudbuild/sonar.sh
+++ cloudbuild/sonar.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-mvn clean scoverage:check scoverage:report sonar:sonar -Dsonar.login=$_SONAR_TOKEN_SECRET -Dsonar.branch.name=$BRANCH_NAME -Dscoverage.minimumCoverage=$_MIN_COVERAGE -B --fail-at-end
+mvn clean scoverage:check scoverage:report sonar:sonar -Dsonar.login=$_SONAR_TOKEN_SECRET -Dsonar.branch.name=$_BRANCH_NAME -Dscoverage.minimumCoverage=$_MIN_COVERAGE -B --fail-at-end
