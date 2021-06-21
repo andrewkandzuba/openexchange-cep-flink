0e1fe3dc69eba206302d3d1ebb11eeaffa2e5834
andrew_kandzuba <andrey.kandzuba@gmail.com>
2021-06-21 13:21:09 -0500

    fix unit tests

--- noaa/src/test/scala/io.openexchange.noaa/JsonUtils.scala
+++ noaa/src/test/scala/io.openexchange.noaa/JsonUtils.scala
@@ -1,18 +1,22 @@
 package io.openexchange.noaa
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
-import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
+import com.fasterxml.jackson.module.scala.ScalaObjectMapper
 
 object JsonUtils {
   val mapper = new ObjectMapper() with ScalaObjectMapper
   mapper.registerModule(DefaultScalaModule)
+    .registerModule(new Jdk8Module())
+    .registerModule(new JavaTimeModule());
 
   def toJson(value: Any): String = {
     mapper.writeValueAsString(value)
   }
 
-  def fromJson[T](json : String)(implicit m : Manifest[T]) : T = {
-     mapper.readValue[T](json)
+  def fromJson[T](json: String)(implicit m: Manifest[T]): T = {
+    mapper.readValue[T](json)
   }
 }
--- noaa/src/test/scala/io.openexchange.noaa/StationBearingParsingTest.scala
+++ noaa/src/test/scala/io.openexchange.noaa/StationBearingParsingTest.scala
@@ -51,11 +51,11 @@ class StationBearingParsingTest  extends AnyFlatSpec with MockFactory with Match
 
   "Parsed t" should "contains" in {
     val t = JsonUtils.fromJson[StationBearing](jsonRaw)
-    assert("72259513911".eq(t.station))
+    assert("72259513911" == t.station)
   }
 
   "JSON s" should "be equal to" in {
     val s = JsonUtils.toJson(stationBearing)
-    assert("".eq(s))
+    assert("{\"station\":\"72259513911\",\"observationDate\":[2020,6,1,0,52],\"reportType\":\"FM-15\",\"observationSource\":\"7\",\"hourlyDewPointTemperature\":26,\"hourlyDryBulbTemperature\":59,\"hourlyPrecipitation\":0.0,\"hourlyRelativeHumidity\":28.0,\"hourlySeaLevelPressure\":30.04,\"hourlySkyConditions\":\"CLR:00\",\"hourlyStationPressure\":29.4,\"hourlyVisibility\":0.0,\"hourlyWetBulbTemperature\":45,\"hourlyWindDirection\":170,\"hourlyWindGustSpeed\":0,\"hourlyWindSpeed\":7}" == s)
   }
 }
